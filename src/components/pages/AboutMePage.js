94610203aaee4259eb36b590f98927a402ab3fa9
Marflow 159"git config --global user.name Marflow <vladbogachenko.vb@gmail.com>
2023-09-10 21:09:05 +0300

    meta

--- src/components/pages/AboutMePage.js
+++ src/components/pages/AboutMePage.js
@@ -15,9 +15,9 @@ const AboutMePage = () => {
 
             <div className="aboutMePage__items">
                 <img className="rec4" src="img/items/Rectangle1.png" alt="Rectangle4" />
-                <img className="rec5" src="img/items/Rectangle2.png" alt="Rectangle4" />
-                <img className="frame3" src="img/items/frame3.png" alt="frame3" />
-                <img className="frame4" src="img/items/frame.png" alt="frame3" />
+                <img className="rec5" src="img/items/Rectangle2.png" alt="Rectangle5" />
+                <img className="frame3" src="img/items/Frame3.png" alt="frame3" />
+                <img className="frame4" src="img/items/Frame.png" alt="frame4" />
             </div>
         </div>
     )
